54e3b5425ddab5f52fcfc64f7f8c5964cae61813
--global <--global>
2019-03-24 21:49:54 +0100

    code optimisation 2

--- R/fun.vardispo.R
+++
@@ -1,4 +0,0 @@
-.fun.blocksnames<-function(data,listname,myblock){
-  vardispo<-colnames(data)[!colnames(data)%in%unlist(listname[!names(listname)%in%myblock])]
-  return(list(vardispo=vardispo))
-}
--- R/runTHEME.R
+++
@@ -1,8 +0,0 @@
-THEME.UI <- function() {
-  appDir <- system.file("myapp", package = "THEME")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal",launch.browser=TRUE)
-}
